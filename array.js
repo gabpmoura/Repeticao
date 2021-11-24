ac93f1b645a75577cc55e16b7d78ad60d2e46927
GiovanaFerreirah <giovana.ferreirah@gmail.com>
2021-11-24 17:20:52 -0200

    primeiro commit

---
+++ array.js
@@ -0,0 +1,9 @@
+const diasdaSemana = ["Domingo", "Segunda", "terca","Quarta"];
+
+console.log(diasdaSemana[0]);
+console.log(diasdaSemana[1]);
+console.log(diasdaSemana[2]);
+console.log(diasdaSemana[3]);
+console.log(diasdaSemana[4]);
+console.log(diasdaSemana[5]);
+console.log(diasdaSemana[6]);
